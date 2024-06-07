1445a4600aee39b3dc20acdfb8c9c7fff4a44cef
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-07 14:20:17 +0530

    Passing data from one component to another

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Button, Text,View,TextInput} from 'react-native';
-export const Home=()=>{
+export const Home=(props)=>{
+    // console.warn(props.route.params)
+    const{name,age} = props.route.params
     return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <Text style={{fontSize:30}}> Home Screen</Text>
+      <Text style={{fontSize:30}}> Name:{name}</Text>
+      <Text style={{fontSize:30}}> Age:{age}</Text>
     </View>
     )
   }
--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Text,View,TextInput} from 'react-native';
 export const Login=(props)=>{
+    const [name,setName]=useState("")
+    const age=23
     return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <Text style={{fontSize:30}}> Login Screen</Text>
-      <Button title='Go to Home Page' onPress={()=>props.navigation.navigate('Home')} />
+      <TextInput  
+      style={{fontSize:25,borderColor:'#000',borderWidth:2}}
+      placeholder='Enter your name' onChangeText={(text)=>setName(text)}/>
+      <Button title='Go to Home Page' 
+      onPress={()=>props.navigation.navigate('Home',{name,age})} />
     </View>
     )
   }
